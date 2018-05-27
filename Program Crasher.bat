5f380ef4e5d12dc00198b3a5b241660b1ad7f765
Alexander Lynd <alexander.lynd@smhsstudents.org>
2018-05-26 20:45:45 -0700

    Stupid program crasher

---
+++ Program Crasher.bat
@@ -0,0 +1,14 @@
+REM stupid program crasher.  just change the names of the programs
+@echo off
+title Task launcher
+color f0
+set list=teams.exe, outlook.exe
+:l
+for %%i in (%list%) do (
+tasklist /FI "IMAGENAME eq teams.exe" 2>NUL | find /I /N "teams.exe">NUL
+if "%ERRORLEVEL%"=="0" echo Do not close this window attempting re-launch && timeout 10 && taskkill /F /IM %%i 
+echo Error Launching Task "Microsoft Teams"
+timeout 2
+cls
+)
+goto l
