a2ebdbeceb73814e589b412052be15a4fa0ea9f9
AlexLynd <alexlynd@protonmail.com>
2019-06-21 12:57:57 -0700

    winvim was shit

--- WinVim.bat
+++
@@ -1,9 +0,0 @@
-::Crappy text editor made for SKids who want to look cool using the command line
-@echo off
-title WinVim 1
-cls
-set /p name= File Name (include filetype) :
-echo Press Ctrl+z then enter to save.  Repeat to exit program
-echo.
-copy con %name%
-if exist %name% copy %name% + con
